9ce3609f0c18d6de4f88cdc370e88e7914943d31
Thomas Lee <thomaskb.lee@gmail.com>
2025-10-24 02:56:27 -0400

    Edit deployment script

--- packages/contracts/ignition/modules/Deploy.ts
+++ packages/contracts/ignition/modules/Deploy.ts
@@ -8,13 +8,12 @@ const UNISWAP_V3_ROUTER = "0x65669fE35312947050C450Bd5d36e6361F85eC12"; // Unisw
 // Addresses for assets
 const NATIVE_ASSET = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";   // Native Asset = ETH
 const USDC = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";   // USDC in Sepolia
-const USDT = "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0";   // USDT in Sepolia
-const TOKEN_ADDRESSES = [NATIVE_ASSET, USDC, USDT];
+
+const TOKEN_ADDRESSES = [NATIVE_ASSET, USDC];
 
 const DeploymentModule = buildModule("DeploymentModule", (m) => {
 
     const poseidonT3 = m.library("poseidon-solidity/PoseidonT3.sol:PoseidonT3");
-    const poseidonT4 = m.library("poseidon-solidity/PoseidonT4.sol:PoseidonT4");
     const leanImt = m.library("@zk-kit/lean-imt.sol/LeanIMT.sol:LeanIMT", {
         libraries: {
             PoseidonT3: poseidonT3, 
@@ -26,7 +25,7 @@ const DeploymentModule = buildModule("DeploymentModule", (m) => {
         // Deploy libraries before contract
         libraries: {
             LeanIMT: leanImt,
-            PoseidonT4: poseidonT4,
+            PoseidonT3: poseidonT3,
         }
     });
 
